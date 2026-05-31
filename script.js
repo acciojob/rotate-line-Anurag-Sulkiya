7958f7805473433a7fcc58adac7b1ccc07f97719
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2026-05-31 10:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+var rotationStep = 2;
+var updateInterval = 20;
+var currentAngle = 0;
+
+var line = document.getElementById("line");
+
+function rotateLine(){
+
+	currentAngle = currentAngle + rotationStep;
+	line.style.transform = "rotate("+currentAngle+"deg)";
+	
+}
+setInterval(rotateLine,20);
